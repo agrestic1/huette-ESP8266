357167a31b4a30eb449d54b89c2e2d60a261f0cc
ksm2abt <martinrene.kessler@de.bosch.com>
2019-12-13 18:32:07 +0100

    + EEPROM storrage of status + Serial.Print now optional  with #define DEBUG

--- ESp8266_socketIO/ESP8266_socketIO.ino
+++ ESp8266_socketIO/ESP8266_socketIO.ino
@@ -2,67 +2,129 @@
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 //#include <ESP8266WebServer.h>
-#include <SocketIoClient.h> // requires SocketIO package by Markus Sattler
+#include <SocketIoClient.h> // requires SocketIoCleint package by Vincent Wyszynski and also WebSockets Package by Markus Sattler
+#include <EEPROM.h> // necessary to store and read values from Flash (virtual EEPORM)
 #include "privates.h" // conrains your WiFi SSID and PW, must be adjusted
 // content of privates.h:
 // const char* ssid = "yourSSID";
 // const char* password = "yourPW";
 
 SocketIoClient Socket;
+#define DEBUG // DEBUG Option prints all that happens to serial Monitor
+
+#ifdef DEBUG
+ #define DEBUG_PRINT(x)     Serial.print (x)
+ #define DEBUG_PRINTLN(x)  Serial.println (x)
+#else
+ #define DEBUG_PRINT(x)
+ #define DEBUG_PRINTLN(x)
+#endif 
+
+#define PWM_PIN 13 // Pin to Output, 13 is LED_BUILTIN
+#define EEPROM_size 1
+// Globals
+const int EEPROM_adr_bright = 255; // EEPROM Adress of brightness
+int bright = 255; // Brightness, initialize to off (=255)
 
 IPAddress staticIP(192, 168, 178, 22); // ESP8266's static IP adress
 IPAddress gateway(192, 168, 178, 1); // Your Gateway
-IPAddress subnet(255, 255, 255, 0);
+IPAddress subnet(255, 255, 255, 0); // Your Subnet mask
 
+// TODO: change to JSON-based communication
 void event(const char * payload, size_t lenght) {
-        String text = String((char *) &payload[0]);
-        if (text == "LED1") {
-          digitalWrite(LED_BUILTIN, LOW);
-          Serial.println("led just lit");
-          //webSocket.sendTXT(num, "led just lit", lenght);
-          Socket.emit("plainString", "\"this is a plain string\"");
-        }
-        if (text == "LED0") {
-          digitalWrite(LED_BUILTIN, HIGH);
-          Serial.println("led just lit");
-          //webSocket.sendTXT(num, "led just lit", lenght);
-        }
-        if(text.startsWith("D")){
-            String xVal=(text.substring(text.indexOf("D")+1,text.length())); // remove D from string
-            int xInt = xVal.toInt();
-            analogWrite(LED_BUILTIN,xInt); 
-            Serial.println(xVal);
-            //webSocket.sendTXT(num, "brightness changed", lenght);
-           }
+  String text = String((char *) &payload[0]);
+  if (text == "LED1") {
+    LAMP_update();
+    DEBUG_PRINTLN("led just lit");
+  }
+  if (text == "LED0") {
+    LAMP_update();
+    DEBUG_PRINTLN("led just lit");
+    }
+  if(text.startsWith("D")){
+      String xVal=(text.substring(text.indexOf("D")+1,text.length())); // remove D from string
+      bright = xVal.toInt();   
+      if (bright > 255){ // respect range
+        DEBUG_PRINTLN("Brightness value reduced from " + xVal + " to 255");
+        bright = 255;
+      } else if (bright < 0){ // respect range
+        DEBUG_PRINTLN("Brightness value increased from " + xVal + " to 0");
+        bright = 0;
+      }
+      bright = 255 - bright; // Range from 0 (off) to 255 (full brightness) instad of other way round
+      EEPROM_update_write();
+      LAMP_update(); 
+      }
+}
 
+void LAMP_update(){
+  analogWrite(PWM_PIN,bright); 
+  DEBUG_PRINTLN("Brightness changed to ");
+  DEBUG_PRINT(bright);
+}
 
-      //webSocket.sendTXT(num, payload, lenght);
-      //webSocket.broadcastTXT(payload, lenght);
+bool EEPROM_update_write() { // Writes all current values to EEPROM
+  bool success = false;
+  EEPROM.write(EEPROM_adr_bright, bright);
+  if (EEPROM.commit()) {
+    DEBUG_PRINTLN("EEPROM successfully committed");
+    success = true;
+  } else {
+    DEBUG_PRINTLN("ERROR! EEPROM commit failed");
+  }
+  return success;
+}
+
+bool EEPROM_update_read() { // Writes all values from EEPROM
+  bool success = false;
+  if (true){ // TODO: 
+    success = true;
+    DEBUG_PRINTLN("ERROR read succeeded");
+  } else {
+    DEBUG_PRINTLN("ERROR read failed.");
+  }
+  bright = EEPROM.read(EEPROM_adr_bright);
+  return success;
 }
 
 void sendType(const char * payload, size_t lenght){ // const char * payload, size_t leght necessary!
   Socket.emit("jsonObject", "{\"Type\":\"LED\"}");
 }
 
+void EEPROM_clear_all(){
+  EEPROM.end();
+  EEPROM.begin(512);
+  // write a 0 to all 512 bytes of the EEPROM
+  for (int i = 0; i < 512; i++) {
+    EEPROM.write(i, 0);
+  }
+  EEPROM.end();
+  EEPROM.begin(EEPROM_size);
+}
+
 void setup(void) {
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(PWM_PIN, OUTPUT);
+  analogWriteRange(255); // use 8 bit range instead of 10 bit so it's easier to handle
+  digitalWrite(PWM_PIN, LOW);
   Serial.begin(115200);
+  EEPROM.begin(EEPROM_size); // [byte] Set size of EEPROM up to 512
+  EEPROM_update_read(); // update status from EEPROM
+  LAMP_update(); // Set lamp to status read from EEPROM
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   WiFi.config(staticIP, gateway, subnet); // comment this line to use DHCP instead of static IP
-  Serial.println("");
+  DEBUG_PRINTLN("");
 
   // Wait for connection
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
+    DEBUG_PRINT(".");
   }
-  Serial.println("");
-  Serial.print("Connected to ");
-  Serial.println(ssid);
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
+  DEBUG_PRINT("Connected to ");
+  DEBUG_PRINTLN(ssid);
+  DEBUG_PRINT("IP address: ");
+  DEBUG_PRINTLN(WiFi.localIP());
+
 
   const char* host = "192.168.178.69";
   int port = 8080;
@@ -73,4 +135,5 @@ void setup(void) {
 
 void loop(void) {
   Socket.loop();
+  // need to check wifi status constantly?
 }
