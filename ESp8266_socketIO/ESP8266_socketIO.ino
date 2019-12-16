70f736f0d5db6809d77c01a6033e5aa327c0a1e6
fabian.ludwig@iontis.de <fabian.ludwig@iontis.de>
2019-12-16 22:15:03 +0100

    cleanup

--- ESp8266_socketIO/ESP8266_socketIO.ino
+++ ESp8266_socketIO/ESP8266_socketIO.ino
@@ -141,7 +141,13 @@ class DeviceData {
       this->data.on_state = false;
       // 50% Brightness
       this->data.brightness = 50;
+
+      // Todo: Manage EEPROM
+
+      // EEPROM
+      EEPROM.begin(EEPROM_size); // [byte] Set size of EEPROM up to 512
       // EEPROM_read_all(&this->data);
+
       updateOutput();
     };
 
@@ -150,8 +156,10 @@ class DeviceData {
       DeserializationError error = deserializeJson(doc, jsonString, (size_t)MAX_JSON_SIZE);
       // Test if parsing succeeds.
       if (error) {
-        Serial.print(F("deserializeJson() failed: "));
-        Serial.println(error.c_str());
+        // Serial.print(F("deserializeJson() failed: "));
+        // Serial.println(error.c_str());
+        DEBUG_PRINT("deserializeJson() failed: ");
+        DEBUG_PRINTLN(error.c_str());
 
         // Todo: Send error message over to backend using Socket.emit
         // Better: Define other error levels (-2, -3 ..) To indicate type of error (using enum)
@@ -211,8 +219,10 @@ class DeviceData {
       size_t count = serializeJson(doc, jsonString, len);
       // Test if parsing succeeds.
       if (count == 0) {
-        Serial.print(F("serializeJson() failed: "));
-        Serial.println("No bytes written");
+        // Serial.print(F("serializeJson() failed: "));
+        // Serial.println("No bytes written");
+        DEBUG_PRINT("serializeJson() failed: "));
+        DEBUG_PRINTLN("No bytes written");
 
         // Todo: Send error message over to backend using Socket.emit
         // Better: Define other error levels (-2, -3 ..) To indicate type of error (using enum)
@@ -254,6 +264,10 @@ void set(const char * payload, size_t length) {
   handleCommand(payload, length, COMMAND_SET);
 }
 
+void publish(const char* payload, size_t length) {
+  // Todo: build JSON string from current setup and emit over socket
+}
+
 void write_eeprom(const char * payload, size_t length) {
   handleCommand(payload, length, COMMAND_WRITE_EEPROM);
 }
@@ -279,26 +293,20 @@ void handleCommand(const char * payload, size_t length, CommandOptions option) {
   // From here on we use our 'commandBuffer' instead of 'payload', since payload is just a pointer
   // that we are not in control of. Additionally - and for this reason - we can disregard the original 'const' qualifier
   DEBUG_PRINTLN(commandBuffer);
-  // No need for variable here, we just want to have something human readable.. using enum
-  // uint8_t option = 2; // Choose set option
-  // DataPacket_t data; // Initialize empty data packet which will be filled
-  // We intend to deserialize our JSON String to our global 'states' struct.. no need for local data packet
+  // Attempt to deserialize received JSON string
   int success = states->Deserialize(doc, (const char*)commandBuffer); // Option 2 for "set"
   if (success < 0) {
     Socket.emit("error", "{Error deserializing JSON string.}"); // Send error
   } else {
-    DEBUG_PRINTLN("Deserialization succeeded");
-    // Todo: we need a serialize function that is able to generate a 'emittable' JSON String from our struct 'data'
-    // The following will probably not work, would have to move every single property...
-    // states = data; // Sore it to global "states"
-
     // If deserialization worked, let's head to parsing our JSON document now..
+    DEBUG_PRINTLN("Deserialization succeeded");
+    // Parse the resulting JSON document
     success = states->Parse(doc, option);
     if (success < 0) {
       Socket.emit("error", "{Error parsing data.}"); // Send error
     } else {
-      DEBUG_PRINTLN("Parsing succeeded");
       // At this point we should have a beautiful JSON Document already containing our response
+      DEBUG_PRINTLN("Parsing succeeded");
       // Serialize our response (produces JSON String)
       success = states->Serialize(doc, commandBuffer);
       if (success < 0) {
@@ -324,117 +332,16 @@ void handleCommand(const char * payload, size_t length, CommandOptions option) {
 
   // Free the allocated memory, we don't need it anymore
   free(commandBuffer);
-
-  // Our global singleton 'states' instance takes care of itself..
-  // LAMP_update(); // Update output
 }
-
-// void get(const char * payload, size_t lenght) {
-//   // Since our received payload may be overridden by a subsequent command, we have to store it in a fresh buffer here!
-//   // First dynamically allocate 'length' bytes of memory on the stack, we have to free this memory later!
-//   char* commandBuffer = (char*)malloc(length*sizeof(char));
-//   if(commandBuffer == NULL) {
-//     // Error, we were unable to get the requested amount of memory :-(
-//     Socket.emit("Error in set. Memory allocation failed."); // Send error
-//     return;
-//   }
-//   // Copy the payload to our buffer (using strncpy ensures we won't have a buffer overflow)
-//   strncpy(commandBuffer, payload, lenght);
-//   // Though we have to ensure that our string is null terminated, so we simply set the last element to '\0'
-//   // (requirement of 'deserializeJson()')
-//   commandBuffer[lenght-1] = '\0';
-
-//   // From here on we use our 'commandBuffer' instead of 'payload', since payload is just a pointer
-//   // that we are not in control of. Additionally - and for this reason - we can disregard the original 'const' qualifier
-//   DEBUG_PRINTLN(commandBuffer);
-//   // No need for variable here, we just want to have something human readable.. using enum
-//   // uint8_t option = 1; // Choose get option
-//   char json_obj_out[255] = "{}"; // Empty json string, to be filled by deserialize function, will be emitted later
-//   // DataPacket_t data; // Initialize empty data packet which will be filled
-//   int success = deserialize(commandBuffer/*, &data*/, COMMAND_GET, json_obj_out); // Option 1 for "get"
-
-//   if (success < 0) {
-//     Socket.emit("Error in get. Error parsing content"); // Send error
-//   } else {
-//     // Todo: we need a serialize function that is able to generate a 'emittable' JSON String from our struct 'data'
-//     Socket.emit("get", json_obj_out); // Send the state out
-//   }
-
-//   // Free the allocated memory, we don't need it anymore (have our 'data' struct now)
-//   free(commandBuffer);
-// }
-
-// ---------------- JSON -----------------------
-// int deserialize(const char * json_obj/*, DataPacket_t* destination*/, uint8_t option, char* json_obj_out){
-//   // Deserialize the JSON document
-//   DeserializationError error = deserializeJson(doc, json_obj);
-//   // Test if parsing succeeds.
-//   if (error) {
-//     Serial.print(F("deserializeJson() failed: "));
-//     Serial.println(error.c_str());
-
-//     // Todo: Send error message over to backend using Socket.emit
-//     return -1;
-//   }
-  
-//   // Removed indirect access to, anyways, global 'states' DataPacket.. no need for that
-//    switch(option){
-//     case COMMAND_GET: // Get option was chosen
-//       // simply fill the existing doc with requested data
-//       if (doc.containsKey("name"))  doc["name"].set(states->getName()); // Tested
-//       if (doc.containsKey("type"))  doc["type"].set(states->getType()); 
-//       if (doc.containsKey("on_state"))  doc["on_state"].set(states->getOnState()); 
-//       if (doc.containsKey("brightness"))  doc["brightness"].set(states->getBrightness()); 
-//       // serializeJson(doc, json_obj_out); // Tried directly, but does not work
-//       char json_obj_tmp[255]; // shitty, i want to put the infos directly into json_obj_out
-//       serializeJson(doc, json_obj_tmp);
-//       strcpy(json_obj_out, json_obj_tmp);
-//       break;
-//     case COMMAND_SET: // Set option was chosen: Stores values of JSON into destination (i.e. states)
-//       // if (*_id != NULL) *id = *_id;
-//       if (doc.containsKey("name")) states->setName(doc["name"]);
-//       if (doc.containsKey("type"))  states->setType(doc["type"]);
-//       if (doc.containsKey("on_state"))  states->setOnState(doc["on_state"];
-//       if (doc.containsKey("brightness"))  states->setBrightness(doc["brightness"];
-//       break;
-//     case COMMAND_WRITE_EEPROM: // EEPROM option was chosen: Stores values of JSON into EEPROM
-//       if (doc.containsKey("name"))  EEPROM_writeAnything(EEPROM_adr_on_state,doc["name"]);
-//       if (doc.containsKey("type"))  EEPROM_writeAnything(EEPROM_adr_on_state,doc["type"]);
-//       if (doc.containsKey("on_state"))  EEPROM_writeAnything(EEPROM_adr_on_state,doc["on_state"]);
-//       if (doc.containsKey("brightness"))  EEPROM_writeAnything(EEPROM_adr_brightness,doc["brightness"]);
-//       break;
-//     default:
-//       // This should never happen!
-//       // Means we received an invalid/unknown command
-//       break;
-//   }
-//   return 0;
-// }
 
 // ----------------- DEBUG -------------------
 void printState(){
   states->print();
 }
 
-// --------------- SETUP -----------------
-void setup(void) {
-  // GPIO
-  pinMode(PWM_PIN, OUTPUT);
-  analogWriteRange(PWM_RANGE); // use 8 bit range instead of 10 bit so it's easier to handle
-  digitalWrite(PWM_PIN, LOW);
-    // EEPROM
-  EEPROM.begin(EEPROM_size); // [byte] Set size of EEPROM up to 512
-  // Have to use different approach
-  // #ifdef SETUP
-  //   set(json,sizeof(json));
-  //   EEPROM_write_all(&states);
-  // #endif
-  // See constructor of 'states'
- //EEPROM_read_all(&states); // update status from EEPROM
-  // LAMP_update(); // Set lamp to status read from EEPROM
-  states = new DeviceData();
-
-  // Serial
+void setupSerial() {
+// Serial
+#ifdef DEBUG
   Serial.begin(115200);
   #ifdef DEBUG
     while (!Serial){ // Wait for serial connection
@@ -445,13 +352,20 @@ void setup(void) {
     } 
     DEBUG_PRINTLN("Default Values:");
     printState();
-  #endif
+#endif
+}
 
-  // TEST:
-  // deserialize(json,2);
-  // get(json,sizeof(json));
+// Outsourced setup routines
 
-    // WiFi
+void setupPeripherals() {
+  // GPIO
+  pinMode(PWM_PIN, OUTPUT);
+  analogWriteRange(PWM_RANGE); // use 8 bit range instead of 10 bit so it's easier to handle
+  digitalWrite(PWM_PIN, LOW);
+}
+
+void setupWifi() {
+  // WiFi
   WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
   DEBUG_PRINTLN("Starting WiFi init");
@@ -463,18 +377,28 @@ void setup(void) {
   DEBUG_PRINTLN(ssid);
   DEBUG_PRINT("IP address: ");
   DEBUG_PRINTLN(WiFi.localIP());
+}
+
+
+// --------------- SETUP -----------------
+void setup(void) {
+  setupSerial();
+  setupPeripherals();
+  setupWifi();
+
+  states = new DeviceData();
 
   // Socket
   Socket.begin(HOST, PORT); // Connected to Device Socket using config from config.h
   // Subscriptions:
-  //Socket.on("connect", sendType); // Connected to server
-  Socket.on("get", get); // get event emits states of this device
-  Socket.on("set", set); // set event changes states on this device
-  Socket.on("write_eeprom", write_eeprom); // writes new default states into EEPROM
-  // TODO: catch unknown event
+  Socket.on("get", get); // get selected properties from this device
+  Socket.on("set", set); // set selected properties on this device
+  Socket.on("publish", publish); // publish all properties 
+  Socket.on("write_eeprom", write_eeprom); // writes selected properties to EEPROM as default settings
+
 }
 
 void loop(void) {
   Socket.loop();
-  // need to check wifi status constantly?
+  // Todo: need to check wifi status constantly?
 }
